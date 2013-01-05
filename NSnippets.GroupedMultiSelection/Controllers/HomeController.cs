f4d74a63ab2ef7b4d8801bc12338883092d2bad2
Constantin Titarenko <constantin.titarenko@gmail.com>
2013-01-06 01:27:56 +0200

    Removed unused code.

--- NSnippets.GroupedMultiSelection/Controllers/HomeController.cs
+++ NSnippets.GroupedMultiSelection/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Web.Mvc;
 using NSnippets.GroupedMultiSelection.Models;
-using NSnippets.GroupedMultiSelection.Infrastructure;
 
 namespace NSnippets.GroupedMultiSelection.Controllers
 {
@@ -11,12 +10,12 @@ namespace NSnippets.GroupedMultiSelection.Controllers
             return View(new ViewModel
             {
                 Ints = new MultiSelection<int>(
-                    viewModel.Ints.SafeGet(x => x.Selected),
+                    viewModel.Ints.Selected,
                     new MultiSelectionItem<int>("From 1 to 3", 1, 2, 3),
                     new MultiSelectionItem<int>("From 5 to 8", 5, 6, 7, 8),
                     new MultiSelectionItem<int>("Just 9", 9)),
                 Strings = new MultiSelection<string>(
-                    viewModel.Strings.SafeGet(x => x.Selected),
+                    viewModel.Strings.Selected,
                     new MultiSelectionItem<string>("Just A", "A"),
                     new MultiSelectionItem<string>("Z and Y", "Z", "Y"))
             });
--- NSnippets.GroupedMultiSelection/Models/ViewModel.cs
+++ NSnippets.GroupedMultiSelection/Models/ViewModel.cs
@@ -5,5 +5,11 @@
         public MultiSelection<int> Ints { get; set; }
 
         public MultiSelection<string> Strings { get; set; }
+
+        public ViewModel()
+        {
+            Ints = new MultiSelection<int>();
+            Strings = new MultiSelection<string>();
+        }
     }
 }
